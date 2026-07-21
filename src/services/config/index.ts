5cb24466a0a54af031e4d18de98bacca8639a426
Henrique Pedroso <133551506+HenriquePedrosoTech@users.noreply.github.com>
2026-07-21 12:23:09 -0300

    Update index.ts

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -2,14 +2,11 @@ import axios, { type AxiosInstance } from "axios"
 import { useAuthStore } from "../stores/auth"
 
 export class AxiosConfig {
-  // Tipamos o $instance para facilitar o uso depois
+ 
   private $instance: AxiosInstance
 
   constructor(public baseURL: string = "/api") {
-    // Como você já definiu 'public baseURL' no construtor,
-    // a linha 'this.baseURL = baseURL' é redundante,
-    // mas deixei como você queria para silenciar o erro.
-    // eslint-disable-next-line
+    
     this.baseURL = baseURL
 
     this.$instance = axios.create({
@@ -26,7 +23,6 @@ export class AxiosConfig {
       return config
     })
 
-    // CORREÇÃO: Removi o parêntese extra que estava após o (res)
     this.$instance.interceptors.response.use((res) => {
       return res
     })
