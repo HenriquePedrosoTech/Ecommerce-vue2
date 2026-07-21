0c0ed33c24a30027f217c8690cd9dac81defb22d
Henrique Pedroso <133551506+HenriquePedrosoTech@users.noreply.github.com>
2026-07-21 12:23:40 -0300

    Update correios.rest.ts

--- src/services/rest/correios.rest.ts
+++ src/services/rest/correios.rest.ts
@@ -2,7 +2,7 @@ import { httpClient } from "../config/config";
 
 export class productRest {
   getAll(params: any) {
-    // Voltamos para a rota base, mas adicionando a barra para evitar o conflito de UUID
+    
     const baseUrl = "/correios"
     const path = `/cep`
     return httpClient.get(path, params, baseUrl)
