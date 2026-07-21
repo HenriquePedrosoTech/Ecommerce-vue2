da7869f117f8b9da00918297950b35268da542b9
Henrique Pedroso <133551506+HenriquePedrosoTech@users.noreply.github.com>
2026-07-21 12:24:00 -0300

    Update product.rest.ts
    Removed commented line explaining the base route.

--- src/services/rest/product.rest.ts
+++ src/services/rest/product.rest.ts
@@ -2,7 +2,7 @@ import { httpClient } from "../config/config";
 
 export class productRest {
   getAll(params: any) {
-    // Voltamos para a rota base, mas adicionando a barra para evitar o conflito de UUID
+   
     const path = "/products/"
     return httpClient.get(path, params)
   }
